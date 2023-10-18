6006e7578c04ac3313bdaca3c18724388c31b42b
Kasun Purnima <kasunpurnima89@gmail.com>
2023-10-18 13:08:41 +0530

    Inital

---
+++ src/main/java/com/example/log4j2sample/Log4jSampleApplication.java
@@ -0,0 +1,29 @@
+package com.example.log4j2sample;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication
+@EnableSwagger2
+public class Log4jSampleApplication implements ApplicationRunner {
+	private static final Logger logger = LogManager.getLogger(Log4jSampleApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jSampleApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		/*logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Fatal error. Please fix me.");*/
+	}
+}
